d91470779c8cc101f2b9ccd4a98ba37a335acfba
ThrawnCA <shell_layer-github@yahoo.com.au>
2019-04-17 09:13:41 +1000

    generate PDF with Mutool if present, github #6

--- build-epub.sh
+++ build-epub.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-ARTIFACT="Harry Potter and the Nightmares of Futures Past.epub"
+ARTIFACT_NAME="Harry Potter and the Nightmares of Futures Past"
+ARTIFACT="$ARTIFACT_NAME.epub"
 DIR=`dirname $0`
 
 mkdir -p $DIR/target
@@ -11,3 +12,10 @@ for file in `ls $DIR/OEBPS/*ml $DIR/OEBPS/*.ncx $DIR/OEBPS/*.opf`; do
   sed 's|\s\s\s*| |g' "$file" | tr -d '\n' > `echo $file | sed "s|$DIR/OEBPS/|$DIR/target/OEBPS/|"`
 done
 (cd target && zip -X0 "$ARTIFACT" mimetype && zip -r "$ARTIFACT" META-INF OEBPS)
+
+if (which mutool 2>/dev/null); then
+  echo "Converting to PDF..."
+  (cd target && mutool convert -o "$ARTIFACT_NAME.pdf" "$ARTIFACT")
+else
+  echo "No PDF converter found. Consider installing MuPDF Tools."
+fi
