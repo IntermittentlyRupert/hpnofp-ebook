292684b36cb74fed3a0a1b911ff27d236534d921
Ian Moriarty <ian@ianjmoriarty.com>
2019-04-26 20:50:12 +1000

    Make build.sh less noisy

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ clean () {
 }
 
 minify_resources () {
+  echo "Minifying resources..."
   mkdir -p "$MINIFIED_DIR"
   cp -r "$INPUT_DIR/mimetype" "$INPUT_DIR/META-INF" "$INPUT_DIR/OEBPS" "$MINIFIED_DIR/"
   for file in `ls $INPUT_DIR/OEBPS/*ml $INPUT_DIR/OEBPS/*.ncx $INPUT_DIR/OEBPS/*.opf`; do
@@ -22,6 +23,7 @@ minify_resources () {
 }
 
 make_epub () {
+  echo "Generating ePub..."
   SRC_DIR="$1"
   if [ $# -lt 2 ]; then
     DEST_FILE="$EPUB_ARTIFACT"
@@ -30,13 +32,13 @@ make_epub () {
   fi
   # we need to force a non-relative path here
   CANONICAL_DEST_FILE="$(cd "$(dirname "$DEST_FILE")"; pwd)/$(basename "$DEST_FILE")"
-  (cd $SRC_DIR && zip -X0 "$CANONICAL_DEST_FILE" mimetype && zip -r "$CANONICAL_DEST_FILE" META-INF OEBPS)
+  (cd $SRC_DIR && zip -q -X0 "$CANONICAL_DEST_FILE" mimetype && zip -q -r "$CANONICAL_DEST_FILE" META-INF OEBPS)
 }
 
 to_mobi () {
-  if (which ebook-convert 2>/dev/null); then
+  if (which ebook-convert >/dev/null 2>&1); then
     echo "Converting to MOBI using Calibre ebook-convert..."
-    ebook-convert "$EPUB_ARTIFACT" "$OUTPUT_DIR/$ARTIFACT_NAME.mobi"
+    ebook-convert "$EPUB_ARTIFACT" "$OUTPUT_DIR/$ARTIFACT_NAME.mobi" > /dev/null
   else
     echo "No MOBI converter found. Consider installing Calibre."
     return 1
@@ -44,17 +46,17 @@ to_mobi () {
 }
 
 to_pdf () {
-  if (which ebook-convert 2>/dev/null); then
+  if (which ebook-convert >/dev/null 2>&1); then
     echo "Converting to PDF using Calibre ebook-convert..."
     # try to use a Garamond font, otherwise just a Serif font
     SERIF_FONT=$(fc-list : family |grep "Garamond" |cut -d , -f 1 |head -1)
     if [ -z "$SERIF_FONT" ]; then
       SERIF_FONT=Serif
     fi
-    ebook-convert "$EPUB_ARTIFACT" "$OUTPUT_DIR/$ARTIFACT_NAME.pdf" --paper-size a4 --pdf-page-numbers --pdf-serif-family "$SERIF_FONT" --pdf-standard-font serif
-  elif (which mutool 2>/dev/null); then
+    ebook-convert "$EPUB_ARTIFACT" "$OUTPUT_DIR/$ARTIFACT_NAME.pdf" --paper-size a4 --pdf-page-numbers --pdf-serif-family "$SERIF_FONT" --pdf-standard-font serif > /dev/null
+  elif (which mutool >/dev/null 2>&1); then
     echo "Converting to PDF using Mutool..."
-    mutool convert -o "$OUTPUT_DIR/$ARTIFACT_NAME.pdf" "$EPUB_ARTIFACT"
+    mutool convert -o "$OUTPUT_DIR/$ARTIFACT_NAME.pdf" "$EPUB_ARTIFACT" > /dev/null
   else
     echo "No PDF converter found. Consider installing Calibre or MuPDF Tools."
     return 1
@@ -96,6 +98,7 @@ make_target () {
     *)
       echo "Unrecognised build target: $BUILD_TARGET"
   esac
+  echo "Done"
 }
 
 mkdir -p "$OUTPUT_DIR"
